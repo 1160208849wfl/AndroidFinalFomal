54777dea62293c9ba5f5ea3ba07a06193ee7fb96
Allen_Lin_ <1160208849wfl@gmail.com>
2018-12-19 13:40:50 +0800

    修复Music、Video资源Bug
    用户有Client传值改为从session中获取

--- src/main/java/top/wufl/api/MusicAPI.java
+++ src/main/java/top/wufl/api/MusicAPI.java
@@ -6,16 +6,19 @@ import top.wufl.bean.Result;
 import top.wufl.bean.User;
 import top.wufl.service.extend.MusicService;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import java.util.List;
 
 @Path("/android_server")
 public class MusicAPI {
     private MusicService musicService = new MusicService();
-
+    @Context
+    HttpServletRequest request;
     /**
      * 通过用户请求视频资源
      *
@@ -26,15 +29,16 @@ public class MusicAPI {
     @Consumes("application/json; charset=utf-8")
     @Produces("application/json; charset=utf-8")
     @Auth("login")
-    public Result getAllMusic(User user) {
+    public Result getAllMusic() {
         List<Music> videos = null;
+        User user = (User) request.getSession().getAttribute("user");
         String description = "";
         try {
             videos = musicService.findByFieldName("userName", user.getUserName());
-            description = "查询成功";
+            description = "所有音乐资源查询成功";
         } catch (Exception e) {
             System.out.println("数据库错误");
-            description = "查询失败, 数据库错误";
+            description = "音乐资源查询失败, 数据库错误";
         }
         return new Result(0, description, videos, "");
     }
--- src/main/java/top/wufl/api/VideoAPI.java
+++ src/main/java/top/wufl/api/VideoAPI.java
@@ -6,20 +6,23 @@ import top.wufl.bean.User;
 import top.wufl.bean.Video;
 import top.wufl.service.extend.VideoService;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import java.util.List;
 
 @Path("/android_server")
 public class VideoAPI {
     private VideoService videoService = new VideoService();
+    @Context
+    HttpServletRequest request;
 
     /**
      * 通过用户请求视频资源
      *
-     * @param user
      * @return
      */
     @POST
@@ -27,15 +30,16 @@ public class VideoAPI {
     @Consumes("application/json; charset=utf-8")
     @Produces("application/json; charset=utf-8")
     @Auth("login")
-    public Result getAllVideos(User user) {
+    public Result getAllVideos() {
         List<Video> videos = null;
         String description = "";
+        User user = (User) request.getSession().getAttribute("user");
         try {
             videos = videoService.findByFieldName("userName", user.getUserName());
-            description = "查询成功";
+            description = "视频资源查询成功";
         } catch (Exception e) {
             System.out.println("数据库错误");
-            description = "查询失败, 数据库错误";
+            description = "视频资源查询失败, 数据库错误";
         }
         return new Result(0, description, videos, "");
     }
